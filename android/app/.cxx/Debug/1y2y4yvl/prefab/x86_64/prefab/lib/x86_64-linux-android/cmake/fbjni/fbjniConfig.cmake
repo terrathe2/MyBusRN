59ad5f756ad1feb93bb155689e6260a0d44dff5f
Redha Putra <putra.redha@gmail.com>
2025-07-08 15:42:11 +0700

    init react-native v0.79 with cli 18.0.0

---
+++ android/app/.cxx/Debug/1y2y4yvl/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/fbjni/fbjniConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET fbjni::fbjni)
+add_library(fbjni::fbjni SHARED IMPORTED)
+set_target_properties(fbjni::fbjni PROPERTIES
+    IMPORTED_LOCATION "C:/Users/Phincon/.gradle/caches/8.13/transforms/4a124880d7c24ba6b4144cdb5e52969c/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.x86_64/libfbjni.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Phincon/.gradle/caches/8.13/transforms/4a124880d7c24ba6b4144cdb5e52969c/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
